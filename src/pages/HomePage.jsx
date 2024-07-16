5fc077e23d1682f8731bb431b6e9cac2f8877247
lungelom11 <lungelomkhwemte11@gmail.com>
2024-07-16 16:56:00 +0200

    Footer and FixedIcon

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import Hero from "../components/Hero/Hero"
 import Projects from "../components/Projects/Projects"
 import About from "../components/About/About"
 import Contact from "../components/Contact/Contact"
+import Footer from "../components/Footer/Footer";
+import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
+import ScrollToTop from "../components/SocialIcon/ScrollToTop";
 
 const HomePage = () => {
   return (
@@ -10,6 +13,9 @@ const HomePage = () => {
       <Projects />
       <About />
       <Contact />
+      <FixSocialIcon />
+      <Footer />
+      <ScrollToTop />
     </>
   )
 }
